fd3678a7f9882d82bb8ad152fbba1f77d7b830e8
arobida <afrobida@me.com>
2019-03-19 17:55:16 -0700

    intial

---
+++ README.md
@@ -0,0 +1,2 @@
+# Ridiculous Show Finder
+Lets you search for shows(the app is ridiculous not the shows) but I did it using react hooks which is not ridiculous at all
---
+++ src/App.js
@@ -0,0 +1,95 @@
+import React, { useState } from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+const App = () => {
+	const [val, setVal] = useState('');
+	const [shows, setShows] = useState([]);
+
+	const getData = async () => {
+		const res = await fetch(`https://api.tvmaze.com/search/shows?q=${val}`);
+		const data = await res.json();
+		//console.log(data);
+		setShows(data);
+		console.log(shows);
+		return data;
+	};
+
+	const change = e => {
+		const params = [e.target.value];
+		const esc = encodeURIComponent;
+		const query = Object.keys(params)
+			.map(k => esc(k) + esc(params[k]))
+			.join('&');
+		setVal(query);
+		console.log(val);
+	};
+	const submit = e => {
+		e.preventDefault();
+		getData();
+	};
+	return (
+		<div className="App">
+			<header className="App-header">
+				<img src={logo} className="App-logo" alt="logo" />
+				<h2>Search for a show</h2>
+			</header>
+			<form onSubmit={submit}>
+				<input
+					className="search"
+					type="text"
+					placeholder="Type the name of show"
+					onChange={change}
+				/>
+			</form>
+			<div className="container">
+				{shows.length === 0 ? (
+					<p>Waiting...</p>
+				) : (
+					shows.map(i => {
+						const strip = html => {
+							var temporalDivElement = document.createElement('div');
+							temporalDivElement.innerHTML = html;
+							return (
+								temporalDivElement.textContent ||
+								temporalDivElement.innerText ||
+								''
+							);
+						};
+						const item = {
+							key: i.show.id,
+							title: i.show.name,
+							rating: i.show.rating.average,
+							net: i.show.network ? i.show.network.name : 'Unknown',
+							netSite: i.show.network ? i.show.officialSite : null,
+							altText: i.show.name.toString(),
+							imgSrc: i.show.image
+								? i.show.image.medium
+								: 'https://static.tvmaze.com/images/no-img/no-img-portrait-text.png',
+							sum: strip(i.show.summary)
+						};
+						return (
+							<div className="show" key={item.key}>
+								<h1 className="title">{item.title}</h1>
+								<h3 className="rating">Rating: {item.rating}</h3>
+								<img className="pic" alt={item.altText} src={item.imgSrc} />
+								<p>
+									<a
+										href={item.netSite}
+										target="_blank"
+										rel="noopener noreferrer"
+									>
+										{item.net}
+									</a>
+								</p>
+								<p>{item.sum}</p>
+							</div>
+						);
+					})
+				)}
+			</div>
+		</div>
+	);
+};
+
+export default App;
